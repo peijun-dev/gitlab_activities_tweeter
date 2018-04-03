87777a6a700bf32b4ddc58b222f7d3a41971d208
Peijun/ページュン <contact.zouri.music@gmail.com>
2018-04-04 01:20:50 +0900

    make template files

--- gitlab_tweeter/gatapp/views.py
+++ gitlab_tweeter/gatapp/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
-    return HttpResponse('<h1>Hello</h1>')
+    return render(request, 'gatapp/index.html')
