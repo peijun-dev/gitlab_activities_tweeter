c22cfe6240293679edc062c07b1a8d53068ed8b6
Peijun/ページュン <contact.zouri.music@gmail.com>
2018-04-04 05:23:38 +0900

    make model for user

--- gitlab_tweeter/gatapp/models.py
+++ gitlab_tweeter/gatapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    access_token = models.CharField(max_length = 100)
+    access_secret = models.CharField(max_length = 100)
